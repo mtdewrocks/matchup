bc4a07293a9da0fd7e9a35ccdffeec8c24b4fa7f
Shawn Schreier <shawn.schreier13@gmail.com>
2024-05-22 19:32:42 -0500

    Changed engine to python

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ import openpyxl
 
 # Preparing your data for usage *******************************************
 print(os.getcwd())
-df = pd.read_excel("https://github.com/mtdewrocks/matchup/tree/072ac999722ded50e8b2eeb649c75f091a8ecbcb/assets/Pitcher_Season_Stats.xlsx", usecols=["Name", "W", "L", "ERA", "IP", "SO", "WHIP", "GS"])
+df = pd.read_excel("https://github.com/mtdewrocks/matchup/tree/072ac999722ded50e8b2eeb649c75f091a8ecbcb/assets/Pitcher_Season_Stats.xlsx", usecols=["Name", "W", "L", "ERA", "IP", "SO", "WHIP", "GS"], engine="python")
 dfGameLogs = pd.read_excel("https://github.com/mtdewrocks/matchup/tree/072ac999722ded50e8b2eeb649c75f091a8ecbcb/assets/2024_Pitching_Logs.xlsx", usecols=["Name", "Date", "Opp", "W", "L", "IP", "BF", "H", "R", "ER", "HR", "BB", "SO","Pit"])
 df2 = pd.read_excel("assets/Pitcher_Season_Stats.xlsx", usecols=["Name", "W", "L", "ERA", "IP", "SO", "WHIP", "GS"])
 df['K/IP'] = df["SO"]/df["IP"]
