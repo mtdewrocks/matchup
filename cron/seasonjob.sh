6c0d9230e946e9614387d78d8dc77dd195a7ac1a
Shawn Schreier <shawn.schreier13@gmail.com>
2024-05-31 19:21:51 -0500

    Delete cron directory

--- cron/seasonjob.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Change to the directory containing the Python script
-
-# Run the Python script
-python "https://github.com/mtdewrocks/matchup/tree/main/cron/Getting Pitcher Season Stats for Dashboard.py"
