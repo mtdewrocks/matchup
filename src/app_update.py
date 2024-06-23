120d03f10519a30b862a70014a0163a5298f79ea
Shawn Schreier <shawn.schreier13@gmail.com>
2024-06-23 09:07:38 -0500

    Updating daily files

--- src/app_update.py
+++ src/app_update.py
@@ -6,6 +6,7 @@ import openpyxl
 import requests
 from io import BytesIO
 import dash_bootstrap_components as dbc
+import numpy as np
 
 # Preparing your data for usage *******************************************
 
@@ -65,14 +66,15 @@ dfLastWeek = dfLast7[['Name', 'BA']]
 dfLastWeek = dfLastWeek.rename(columns={"BA":"Last Week Average"} )
 
 #Used for the hitter table
-dfHitters = pd.read_excel("https://github.com/mtdewrocks/matchup/raw/main/assets/Combined_Daily_Data.xlsx", usecols=["fg_name", "Savant Name", "Props Name", "Bats", "Batting Order", "Average", "wOBA",
-                                   "ISO", "K%", "BB%", "Baseball Savant Name"])
+dfHitters = pd.read_excel("https://github.com/mtdewrocks/matchup/raw/main/assets/Combined_Daily_Data.xlsx")
 
-print(dfHitters.shape)
+
 dfHittersFinal = dfHitters.merge(dfLastWeek, left_on="Savant Name", right_on="Name", how="left")
 dfHittersFinal = dfHittersFinal.drop(["Name", "fg_name"],  axis=1)
 
 dfProps = pd.read_excel("https://github.com/mtdewrocks/matchup/raw/main/assets/Daily_Props.xlsx", usecols=["Player", "Over Price", "Line", "market", "bookmakers", "Under Price"])
+dfProps["Line"] = dfProps["Line"].astype(float)
+
 dfHitProps = dfProps.query("market == 'hits' and bookmakers=='draftkings'")
 dfHitProps = dfHitProps.rename(columns={"Over Price":"Hits Over", "Line":"Hits Line", "Under Price":"Hits Under"})
 dfHitProps = dfHitProps.drop(["market", "bookmakers"], axis=1)
@@ -83,12 +85,37 @@ dfKProps = dfKProps.drop(["market", "bookmakers"], axis=1)
 
 dfCombinedProps = dfHitProps.merge(dfKProps, on="Player", how="inner")
 
+
 dfHittersProps = dfHittersFinal.merge(dfCombinedProps, left_on="Props Name", right_on="Player", how="left")
 dfHittersProps = dfHittersProps.drop("Props Name", axis=1)
                                                                                                                      #"Pitcher", 
-                                   #"Pitcher Average", "Pitcher K%"])
+
+dfHittersProps['Exclude_K'] = np.where((dfHittersProps["High K Hitter"] == 1) & (dfHittersProps["Pitcher K%"]>.20),1,0)
+dfHittersProps['Exclude_Order'] = np.where(dfHittersProps["Batting Order"] >=7,1,0)
+dfHittersProps['Exclude_BB'] = np.where((dfHittersProps["BB%"] > 12) & (dfHittersProps["Weighted BB% Pitcher"]>.10),1,0)
+dfHittersProps['Exclude_Avg'] = np.where((dfHittersProps["Average"] < .250) | (dfHittersProps["Pitcher Average"]<.250),1,0)
+
+dfHittersProps['Exclude'] = np.sum(dfHittersProps[['Exclude_K', 'Exclude_Order', 'Exclude_BB', 'Exclude_Avg']],axis=1)
+
+print('printing hitters props shape')
+print(dfHittersProps.shape)
 
 
+dfTopMatchups = dfHittersProps.copy()
+dfTopMatchups = dfTopMatchups.query("Exclude==0")
+dfTopMatchups = dfTopMatchups[dfTopMatchups["Hits Line"]<1]
+print(dfTopMatchups.shape)
+
+
+dfHighK = dfHittersProps.copy()
+dfHighK = dfHighK[(dfHighK["High K Hitter"]==1) & (dfHighK["Pitcher K%"]>=.23)]
+#dfHighK = dfHighK.query("('High K Hitter'== 1) and ('Pitcher K%'>=.23")
+dfHighK = dfHighK[dfHighK["Strikeouts Line"]<1]
+
+dfLowK = dfHittersProps.copy()
+
+dfLowK = dfLowK[(dfLowK["K%"]<=15) & (dfLowK["Pitcher K%"]<=18)]
+#dfLowK = dfLowK.query("'Weighted K% Hitter'<=15 & 'Weighted K% Pitcher'<=.15")
 
 #game_log_style = [{'if':{'filter_query': '{ER} > 1', 'column_id':'ER'}, 'backgroundColor':'pink'},{'if':{'filter_query': '{ER} < 1', 'column_id':'ER'}, 'backgroundColor':'blue'}]
 hitter_style = [{'if':{'filter_query': '{Average} < .250', 'column_id':'Average'}, 'backgroundColor':'lightcoral'}, {'if':{'filter_query': '{Average} < 0.200', 'column_id':'Average'}, 'backgroundColor':'darkred'},\
@@ -130,17 +157,27 @@ matchup_tab = html.Div(
              className="row"),
      html.Div([html.Div(dash_table.DataTable(id="splits-table", data=dfSplits.to_dict("records"), style_cell={"textAlign":"center"}),style={"padding-top":"25px"}, className="six columns"),
       html.Div(dcc.Graph(figure={}, id="pcts-graph", style={'display': 'none'}), className="two columns")], className="row"),
-     html.Div(html.P(children="Splits data are the weighted average of 2023 and 2024.", id="splits-note", style={'display':'none', 'font-weight':'bold'}),className="row"),
+     html.Div(html.P(children="Splits data are from 2024.", id="splits-note", style={'display':'none', 'font-weight':'bold'}),className="row"),
      html.Div(html.Div(dash_table.DataTable(id="hitter-table", data=dfHittersFinal.to_dict("records"), style_cell={"textAlign":"center"}, style_data_conditional=hitter_style),style={"padding-top":"25px"}, className="row"))])
 
-image_path = 'assets/fire.jpg'
-html.Img(src=image_path)
+#image_path = 'assets/fire.jpg'
+#html.Img(src=image_path)
 
-hot_hitter_tab = dbc.Container([dbc.Row([html.Img(src=image_path, id="fire", alt="fire", height=25, width=25, style={'display':'block', 'margin':'auto'}),
-                                         html.H1("Hot Hitters", style={'color': 'red', 'fontSize': 40, 'textAlign':'center'})]), dbc.Row(html.H6("Statistics over the last week", style={'fontSize': 20, 'textAlign':'center'})),
+
+
+hot_hitter_tab = dbc.Container([dbc.Row([html.H1("Hot Hitters", style={'color': 'red', 'fontSize': 40, 'textAlign':'center'})]), dbc.Row(html.H6("Statistics over the last week", style={'fontSize': 20, 'textAlign':'center'})),
                                     dbc.Row(dash_table.DataTable(id="hot-hitters", data=dfHot.to_dict("records"), style_cell={"textAlign":"center"}, sort_action="native"))])
 
-tabs = dbc.Tabs([dbc.Tab(matchup_tab, label="Matchup"), dbc.Tab(hot_hitter_tab, label="Hitter")])
+
+
+top_hitter_matchups = dbc.Container([dbc.Row([html.H1("Top Hitter Matchups", style={'color': 'blue', 'fontSize': 40, 'textAlign':'center'})]),
+                                    dbc.Row(dash_table.DataTable(id="top-matchups", data=dfTopMatchups.to_dict("records"), style_cell={"textAlign":"center"}, sort_action="native"))])
+
+
+top_strikeout_matchups = dbc.Container([dbc.Row([html.H1("Top Strikeout Targets", style={'color': 'blue', 'fontSize': 40, 'textAlign':'center'})]),
+                                    dbc.Row(dash_table.DataTable(id="top-strikeouts", data=dfHot.to_dict("records"), style_cell={"textAlign":"center"}, sort_action="native"))])
+
+tabs = dbc.Tabs([dbc.Tab(matchup_tab, label="Matchup"), dbc.Tab(hot_hitter_tab, label="Hot Hitters"), dbc.Tab(top_hitter_matchups, label="Top Hitter Matchups")])
 app.layout = dbc.Row(dbc.Col(tabs))
 
 
@@ -188,6 +225,8 @@ def update_stats(chosen_value):
     dff = df.copy()
     dff = dff[dff["Baseball_Savant_Name"]==chosen_value]
     dfh = dfHittersProps.copy()
+    dfh = dfh[["fg_name", "Savant Name", "Props Name", "Bats", "Batting Order", "Average", "wOBA",
+                                   "ISO", "K%", "BB%", "Baseball Savant Name"]]
     dfh = dfh[dfh["Baseball Savant Name"]==chosen_value]
     dfh = dfh.sort_values(by="Batting Order")
     dfh = dfh.drop(["Baseball Savant Name"], axis=1)
